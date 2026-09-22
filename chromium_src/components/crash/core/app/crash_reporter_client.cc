8e18ca22bdc5c9c972418406f6a956727a6340f2
Ravanoth <ravanoth@proton.me>
2026-09-22 16:55:00 -0500

    disable crash reporting
    Disabled crash reporting to prevent telemetry.

--- chromium_src/components/crash/core/app/crash_reporter_client.cc
+++ chromium_src/components/crash/core/app/crash_reporter_client.cc
@@ -3,12 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#if defined(OFFICIAL_BUILD)
-#define BRAVE_CRASH_REPORTER_CLIENT_GET_UPLOAD_URL \
-  return "https://cr.brave.com";
-#else
+// Disable all crash reporting to prevent telemetry
 #define BRAVE_CRASH_REPORTER_CLIENT_GET_UPLOAD_URL return std::string();
-#endif
 
 #include <components/crash/core/app/crash_reporter_client.cc>
 #undef BRAVE_CRASH_REPORTER_CLIENT_GET_UPLOAD_URL
